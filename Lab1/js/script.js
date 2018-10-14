ef7320cfd2aac6c396c9f6373b4208a07c6ec4ff
Pchab97 <przemek970@spoko.pl>
2018-10-14 22:21:34 +0200

    Ulepszono strone

---
+++ Lab1/js/script.js
@@ -0,0 +1,8 @@
+document.querySelector('button').onclick = function () {
+    /*if (document.querySelector('tekst') === "Tekst 1") {
+        document.textContent('tekst') = 'Tekst 2';
+    } else {
+        document.textContent('tekst') = 'Tekst 1';
+    }*/
+    alert("przycisk działa, ale co dalej?")
+}
