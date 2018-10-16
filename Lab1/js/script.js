199c0bb7d486a4efff2071b299055c865cd01b4a
Pchab97 <przemek970@spoko.pl>
2018-10-16 08:39:20 +0200

    Update strony

--- Lab1/js/script.js
+++ Lab1/js/script.js
@@ -1,7 +1,11 @@
-document.querySelector('button').onclick = function () {
-    if (document.getElementById('tekst').innerHTML === "Tekst 1") {
+function but1() {
+    if (document.getElementById('tekst').innerHTML == "Tekst 1") {
         document.getElementById("tekst").innerHTML = 'Tekst 2';
     } else {
         document.getElementById("tekst").innerHTML = 'Tekst 1';
     }
+}
+
+function but2() {
+    alert("Ten przycisk działa");
 }
